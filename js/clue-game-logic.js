bab250bc2f080c4d25c31b3836cc591ade97d474
benzen89 <salomaa.leo@gmail.com>
2018-04-26 14:41:42 +0300

    Country loop

--- js/clue-game-logic.js
+++ js/clue-game-logic.js
@@ -4,29 +4,68 @@ This is the game logic. Each marker will show a clue to which marker to find nex
 Progress is stored as HTML5 web storage
 */
 
+var current_clue;	//the clue stored in the country marker
+
 function startUpdate(){
 	window.setInterval(update, 1000);
 }
 
 function update(){
-	console.log("update called");
+	console.log(current_clue);
 	checkMarkers();
 }
 
 function checkMarkers(){
 	if(document.querySelector("#denmark").object3D.visible == true){
-		console.log("Denmark detected");
+		if(current_clue == null){
+			setClueTo("Denmark");
+		} else if(current_clue == "Lithuania"){
+			correctAnswer();
+			setClueTo("Denmark");
+		}
 	}
 	if(document.querySelector("#sweden").object3D.visible == true){
-		console.log("Sweden detected");
+		if(current_clue == null){
+			setClueTo("Sweden");
+		} else if(current_clue == "Denmark"){
+			correctAnswer();
+			setClueTo("Sweden");
+		}
 	}
 	if(document.querySelector("#estonia").object3D.visible == true){
-		console.log("Estonia detected");
+		if(current_clue == null){
+			setClueTo("Estonia");
+		} else if(current_clue == "Sweden"){
+			correctAnswer();
+			setClueTo("Estonia");
+		}
 	}
 	if(document.querySelector("#latvia").object3D.visible == true){
-		console.log("Latvia detected");
+		if(current_clue == null){
+			setClueTo("Latvia");
+		} else if(current_clue == "Estonia"){
+			correctAnswer();
+			setClueTo("Latvia");
+		}
 	}
 	if(document.querySelector("#lithuania").object3D.visible == true){
-		console.log("Lithuania detected");
+		if(current_clue == null){
+			setClueTo("Lithuania");
+		} else if(current_clue == "Latvia"){
+			correctAnswer();
+			setClueTo("Lithuania");
+		}
 	}
+}
+
+function cancelClue(){
+	current_clue = null;
+}
+
+function setClueTo(cntry){
+	current_clue = cntry;
+}
+
+function correctAnswer(){
+	console.log("Correct answer!");
 }
